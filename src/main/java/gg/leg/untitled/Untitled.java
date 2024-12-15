8150d8517c69c63878ba90a7215a8b43821e2b03
RyzechDev <ryzechdev@ryzech.net>
2024-12-15 08:13:21 -0600

    initial commit

---
+++ src/main/java/gg/leg/untitled/Untitled.java
@@ -0,0 +1,17 @@
+package gg.leg.untitled;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Untitled extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
